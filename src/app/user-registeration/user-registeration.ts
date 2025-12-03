e9fb732a3ec218bf3bb5a5ce36dd4b7740fb5819
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-12-03 14:59:47 +0500

    update

--- src/app/user-registeration/user-registeration.ts
+++ src/app/user-registeration/user-registeration.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Route, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-registeration',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user-registeration.html',
   styleUrl: './user-registeration.css',
 })
 export class UserRegisteration {
+constructor(private router: Router){}
+
+  signin(){
+    this.router.navigate(['/signin'])
+  }
 
 }
