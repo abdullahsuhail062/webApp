3e6ee0decf7f6c320b79b6b914ba87000b44d8c2
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-12-03 14:45:46 +0500

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { UserRegisteration } from './user-registeration/user-registeration';
+import { Signin } from './signin/signin';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path: '', component: UserRegisteration},{path: 'signin', component: Signin}];
