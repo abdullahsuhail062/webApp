9b5edb792d2f5dddd05fee52ab2c16ce50e498b3
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-12-25 12:41:20 +0500

    update

--- src/app/user-resolver.ts
+++ src/app/user-resolver.ts
@@ -1,27 +1,27 @@
-// import { ResolveFn, Router } from '@angular/router';
-// import { inject, PLATFORM_ID } from '@angular/core';
-// import { isPlatformBrowser } from '@angular/common';
-// import { map, of } from 'rxjs';
-// import { AuthService } from './auth-service';
+import { ResolveFn, Router } from '@angular/router';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { map, of } from 'rxjs';
+import { AuthService } from './auth-service';
 
 
-// export const userResolver: ResolveFn<boolean> = () => {
-//   const auth = inject(AuthService);
-//   const router = inject(Router);
-//   const platformId = inject(PLATFORM_ID);
+export const userResolver: ResolveFn<boolean> = () => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+  const platformId = inject(PLATFORM_ID);
 
-//   // SSR: allow rendering
-//   if (!isPlatformBrowser(platformId)) {
-//     return of(true);
-//   }
+  // SSR: allow rendering
+  if (!isPlatformBrowser(platformId)) {
+    return of(true);
+  }
 
-//   return auth.loadUser().pipe(
-//     map(user => {
-//       if (user) {
-//         return true;
-//       }
-//       router.navigateByUrl('/signin');
-//       return false;
-//     })
-//   );
-// };
+  return auth.loadUser().pipe(
+    map(user => {
+      if (user) {
+        return true;
+      }
+      router.navigateByUrl('/signin');
+      return false;
+    })
+  );
+};
