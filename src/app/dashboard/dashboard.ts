9fc61ad5d1eeadfdae816ad787da2f00393919c3
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-12-28 08:51:23 +0500

    update

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -2,6 +2,7 @@ import { Component, inject, signal } from '@angular/core';
 import { authStore } from '../auth-store';
 import { Router } from '@angular/router';
 import { ProfileDialog } from '../profile-dialog/profile-dialog';
+import { User } from '../models/user';
 
 
 @Component({
@@ -12,6 +13,7 @@ import { ProfileDialog } from '../profile-dialog/profile-dialog';
 })
 export class Dashboard {
 constructor(private router: Router) {}
+user = signal<User>(authStore.user())
 
  showProfile = signal(false);
 
